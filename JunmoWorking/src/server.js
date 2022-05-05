ed3567632c72722e2c1b3397e4eafa9ae3857796
a00700c <a00700c@gmail.com>
2022-05-05 16:42:56 +0900

    docs: app.js server.js 주석 추가

--- JunmoWorking/src/server.js
+++ JunmoWorking/src/server.js
@@ -26,7 +26,7 @@ let roomObj = [
     // },
 ];
 
-function publicRooms(){
+function publicRooms(){ // 현재 개설중인 방의 목록을 알려주는 함수이다. 
     const publicRooms = [];
     for (let i = 0; i < roomObj.length; ++i){
         publicRooms.push(roomObj[i].roomName);
@@ -34,7 +34,7 @@ function publicRooms(){
     return publicRooms;
 }
 
-function publicRoomCount(){
+function publicRoomCount(){ // 현재 개설중인 방의 인원수를 알려주는 함수이다. 
     const publicRoomCount = [];
     for (let i = 0; i < roomObj.length; ++i){
         publicRoomCount.push(roomObj[i].currentCount);
@@ -45,16 +45,16 @@ function publicRoomCount(){
 wsServer.on("connection", (socket) => {
     let myRoomName = null;
     let myNickname = null;
-    wsServer.sockets.emit("room_change", publicRooms(), publicRoomCount());
+    wsServer.sockets.emit("room_change", publicRooms(), publicRoomCount()); // 방에 누군가 들어가거나 나갈 경우 publicRooms, publicCount를 실행한다.
 
-    socket.on("join_room", (roomName, nickname) => {
+    socket.on("join_room", (roomName, nickname) => {    // join_room이 클라이언트 측에서 roomName, nickname을 매개변수로 emit 된 경우 실행한다.
         myRoomName = roomName;
         myNickname = nickname;
 
         let isRoomExist = false;
         let targetRoom = null;
         
-        for (let i = 0; i < roomObj.length; ++i){
+        for (let i = 0; i < roomObj.length; ++i){   // 내가 들어가려는 방이 현재 있다면 그 방을 targetroom으로 한다.
             if(roomObj[i].roomName === roomName){
                 isRoomExist = true;
                 targetRoom = roomObj[i];
@@ -62,7 +62,7 @@ wsServer.on("connection", (socket) => {
             }
         }
 
-        if(!isRoomExist){
+        if(!isRoomExist){   // 내가 들어가려는 방이 없다면 targetRoom을 새로 만든다.
             targetRoom = {
                 roomName,
                 currentCount: 0,
@@ -71,34 +71,33 @@ wsServer.on("connection", (socket) => {
             roomObj.push(targetRoom);
         }
 
-        targetRoom.users.push({
+        targetRoom.users.push({ // targetRoom으로 접속하고 count를 늘려준다.
             socketId: socket.id,
             nickname,
         });
         ++targetRoom.currentCount;
 
-        socket.join(roomName);
-        socket.emit("welcome", targetRoom.users);
+        socket.join(roomName);  // 방에 join 하는 socket.io 함수
+        socket.emit("welcome", targetRoom.users);   // 클라이언트 측으로 targetRooms의 유저 목록을 매개변수로 하는 welcome을 emit한다.
         wsServer.sockets.emit("room_change", publicRooms(), publicRoomCount());
-
     });
-    socket.on("offer", (offer, remoteSocketId, localNickname) => {
-        socket.to(remoteSocketId).emit("offer", offer, socket.id, localNickname);
+    socket.on("offer", (offer, remoteSocketId, localNickname) => {  // 클라이언트 측에서 offer가 emit된 경우
+        socket.to(remoteSocketId).emit("offer", offer, socket.id, localNickname);   // 상대의 소켓id로 본인의 소켓id와 닉네임을 변수로 offer을 emit한다.
     });
-    socket.on("answer", (answer, remoteSocketId) => {
-        socket.to(remoteSocketId).emit("answer", answer, socket.id);
+    socket.on("answer", (answer, remoteSocketId) => {   // 클라이언트 측에서 answer가 emit된 경우
+        socket.to(remoteSocketId).emit("answer", answer, socket.id);    // 상대의 소켓id로 본인의 소켓id를 변수로 answer을 emit한다.
     });
-    socket.on("ice", (ice, remoteSocketId) => {
-        socket.to(remoteSocketId).emit("ice", ice, socket.id);
+    socket.on("ice", (ice, remoteSocketId) => { // 클라이언트 측에서 ice가 emit된 경우
+        socket.to(remoteSocketId).emit("ice", ice, socket.id);  // ice를 emit하여 준다.
     });
-    socket.on("chat", (message, roomName) => {
-        socket.to(roomName).emit("chat", message);
+    socket.on("chat", (message, roomName) => {  // 클라이언트 측에서 chat이 emit된 경우
+        socket.to(roomName).emit("chat", message);  // 나를 제외한 방 전체로 chat을 emit한다.
     });
-    socket.on("disconnecting", () => {
-        socket.to(myRoomName).emit("leave_room", socket.id, myNickname);
+    socket.on("disconnecting", () => {  // disconnecting이 emit된 경우
+        socket.to(myRoomName).emit("leave_room", socket.id, myNickname);    // 내가 접속한 방에 leave_room을 emit한다.
 
         let isRoomEmpty = false;
-        for (let i = 0; i < roomObj.length; ++i){
+        for (let i = 0; i < roomObj.length; ++i){   // 방에서 나가면 user목록에서 나를 빼고 만약 사람이 없어진다면 방을 없앤다.
             if(roomObj[i].roomName === myRoomName){
                 const newUsers = roomObj[i].users.filter(
                     (user) => user.socketId != socket.id
@@ -119,7 +118,7 @@ wsServer.on("connection", (socket) => {
         }
 
     });
-    socket.on("disconnect", () => {
+    socket.on("disconnect", () => { // disconnect가 emit되면 room_change를 emit한다.
         wsServer.sockets.emit("room_change", publicRooms(), publicRoomCount());
     });
 });
