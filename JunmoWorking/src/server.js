4a03af7adb9c7ef9b244f4c51b8a5897abc2da65
a00700c <a00700c@gmail.com>
2022-05-08 19:40:21 +0900

    feat: 김준모 home.pug 에서 ejs로 수정

--- JunmoWorking/src/server.js
+++ JunmoWorking/src/server.js
@@ -4,7 +4,7 @@ import express from "express";
 
 const app = express();
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (_, res) => res.render('home'));
