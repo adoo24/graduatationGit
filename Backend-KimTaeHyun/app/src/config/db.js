df2679f7c5bebbf0319db6005f4008ba3cbf1689
adoo24 <adoo24@naver.com>
2022-05-23 14:05:46 +0900

    feat: capture시 동일인물인지 확인 기능 추가, capture시 저장되는 경로변경

--- Backend-KimTaeHyun/app/src/config/db.js
+++ Backend-KimTaeHyun/app/src/config/db.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection({
     host : 'localhost',
     port : '3306',
     user: 'root',
-    password: 'rlaxogus121@',
+    password: '1234',
     database: 'bemysupervisor',
 })
 db.connect();
