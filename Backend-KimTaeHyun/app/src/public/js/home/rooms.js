ac833993d40452a5e11bd71386f181b8cdd87944
adoo24 <adoo24@naver.com>
2022-05-24 23:15:19 +0900

    충돌 수정

--- Backend-KimTaeHyun/app/src/public/js/home/rooms.js
+++ Backend-KimTaeHyun/app/src/public/js/home/rooms.js
@@ -21,6 +21,8 @@ let myPeerConnection;
 let peopleInRoom = 1;
 let auth = "";
 let schoolid = "";
+let path1 = ""
+let path2 = ""
 
 let pcObj = {
 
@@ -187,7 +189,55 @@ const dataURLtoFile = (dataurl, fileName) => {
 
 
 
+captureBtn.addEventListener("click", async function() {
+    canvas.getContext('2d').drawImage(myFace, 0, 0, canvas.width, canvas.height);
+    let image_data_url = canvas.toDataURL('image/jpeg');
+    var file = dataURLtoFile(image_data_url, 'capture.jpg');
+    Promise.all([
+        socket.emit("capture", file),
+        faceapi.nets.faceRecognitionNet.loadFromUri('js/home/models'),
+        faceapi.nets.ssdMobilenetv1.loadFromUri('js/home/models'),
+        faceapi.nets.faceLandmark68Net.loadFromUri('js/home/models'),
+    ])
+        .then(start)
 
+    async function start() {
+        let image = await faceapi.fetchImage('capture/'+schoolid+' '+nickname+'.jpg')
+        const labeledFaceDescriptors = await loadLabeledImages()
+        const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.45)
+        console.log("Model Loaded")
+        const singleResult = await faceapi
+        .detectSingleFace(image)
+        .withFaceLandmarks()
+        .withFaceDescriptor()
+        if (singleResult) {
+            const bestMatch = faceMatcher.findBestMatch(singleResult.descriptor)
+            if (bestMatch.label=="Junseo")              //유동적으로 바뀌게 수정해야함
+                alert("사진과 일치합니다.")
+            else
+                alert("사진과 일치하지 않습니다.")
+        } else {
+            alert("얼굴이 감지되지 않습니다. 다시 한번 캡처해주세요.")
+        }
+    }
+
+    function loadLabeledImages() {
+        const labels = ["Junseo"]
+        return Promise.all(
+            labels.map(async label => {
+                    const descriptions = []
+                    let img = await faceapi.fetchImage(path1); //회원가입했을때사진
+                    let detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+                    descriptions.push(detections.descriptor)
+                    img = await faceapi.fetchImage(path2); //회원가입했을때사진
+                    detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+                    descriptions.push(detections.descriptor)
+                    return new faceapi.LabeledFaceDescriptors(label, descriptions)
+                }
+            )
+        )
+    }
+});
 
 
 
@@ -312,10 +362,17 @@ leaveBtn.addEventListener("click", leaveRoom);
 
 // Socket Code
 
-socket.on("info", async(myId, myNickname, myAuth) => {
+socket.on("info", async(myId, myNickname, myAuth, myPath1, myPath2) => {
     schoolid = myId;
     nickname = myNickname;
     auth = myAuth;
+    path1 = myPath1;
+    path2 = myPath2;
+    path1 = path1.replace(/\\/g, '/');
+    path2 = path2.replace(/\\/g, '/');
+    path1 = path1.substr(11);
+    path2 = path2.substr(11);
+
     const nicknameContainer = document.querySelector("#userNickname");
     nicknameContainer.innerText = nickname;
 });
