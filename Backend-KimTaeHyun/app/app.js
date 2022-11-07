7c5092ddc037052a61aac044a880ce57a5b8ef63
bradbrad97 <taehyun0121@naver.com>
2022-11-07 15:18:49 +0900

    세션 쿠키 설정

--- Backend-KimTaeHyun/app/app.js
+++ Backend-KimTaeHyun/app/app.js
@@ -40,11 +40,17 @@ app.use(bodyParser.json());
 // URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
+app.enable('trust proxy');
+
 app.use(expressSession({
     secret : 'my key',
     resave : false,
     saveUninitialized: true,
     store: new FileStore(),
+    cookie: {
+        domain: 'bemysupervisor.com', path:'/',
+        expires: new Date(Date.now() + 3600000)
+    }
 }));
 //라우팅
 const home = require("./src/routes/home");
